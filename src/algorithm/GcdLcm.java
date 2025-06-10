6494a299acc824a74e8be3ee796d23fbff2e8154
taewaem <monkey587@naver.com>
2025-06-10 14:00:00 +0900

    [GcdLcm]

--- src/algorithm/GcdLcm.java
+++ src/algorithm/GcdLcm.java
@@ -1,28 +1,27 @@
-    /*
-    GCD = 최대공약수
-    LCm = 최소공배수
+package algorithm;
 
-     */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 
-    package algorithm;
 
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.nio.Buffer;
+/**
+ *  GCD = 최대공약수
+ *  LCM = 최소공배수
+ */
+public class GcdLcm {
 
-    public class GcdLcm {
-
-        public static int gcd(int a, int b){
-            if(b==0)
-                return a;
-            else
-                return gcd(b, a%b);
-        }
-
-        public static int lcm(int a, int b){
-            //두 수의 곱 / 두 수의 최대공약수
-            return a*b / gcd(a,b);
-        }
-        
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
     }
+
+    public static int lcm(int a, int b) {
+        //두 수의 곱 / 두 수의 최대공약수
+        return a * b / gcd(a, b);
+    }
+
+}
